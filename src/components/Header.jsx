3ec2688b377fa43f2539295ba4248e8dbd31de8b
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-03-31 04:12:42 +0100

    frontend 70% completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,29 @@ import { ShoppingCart, User } from 'lucide-react'
 
 const Header = () => {
     return (
-        <div className=' sticky top-0 w-full bg-[#ffffff]/95 backdrop-blur-lg border-b border-white/20 py-4 flex gap-4 justify-around shadow-sm'>
-            
+        <div className=' sticky top-0 w-full bg-white/40 backdrop-blur-lg border-b border-white/20 py-4 flex gap-4 justify-around items-center shadow-sm'>
+
 
             <div>
-                <Link to="/" className='flex gap-4'><ShoppingCart /> TechStore</Link>
+                <Link to="/" className='flex gap-2'>
+                    <div className='bg-linear-to-r from-blue-500 to-purple-500  rounded-lg p-1'>
+                        <ShoppingCart className='text-white size-5' />
+                    </div>
+                    TechStore
+                </Link>
             </div>
 
             <div className='flex gap-4'>
-                <Link to="/" className='hover:text-blue-600 text-gray-800 transition-colors'>Home</Link>
-                <Link to="/about" className='hover:text-blue-600 text-gray-800 transition-all'>About</Link>
-                <Link to="/contact" className='hover:text-blue-600 text-gray-800 transition-all'>Contact</Link>
+                <Link to="/" className='text-gray-800 '>Home</Link>
+                <Link to="/about" className=' text-gray-800'>About</Link>
+                <Link to="/contact" className=' text-gray-800'>Contact</Link>
             </div>
 
-            <div className='flex gap-4'>
-                <Link to="/login" className=' flex gap-4 hover:text-blue-600 hover:bg-blue-100 rounded-2xl hover:p-0.5 text-gray-800 transition-all'><User /> Login</Link>
-                <Link to="/signup" className='hover:text-blue-600 text-gray-800 transition-all'>Sign Up</Link>
+            <div className='flex gap-3 items-center'>
+                <Link to="/login" className='flex px-2 py-1 gap-2 rounded-lg hover:bg-gray-200 text-gray-800 '><User /> Login</Link>
+                <Link to="/signup" className='text-white px-2 py-1 bg-black rounded-lg'>Sign Up</Link>
                 <Link>
-                    <ShoppingCart size={25} className='text-gray-800 hover:text-blue-600   transition-all' />
+                    <ShoppingCart size={25} className='text-gray-800' />
                 </Link>
             </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,69 @@
 import React from 'react'
+import { ArrowRight, Shield, Truck, Zap } from "lucide-react"
+
 
 const Hero = () => {
   return (
-    <div>
-        <div className='flex justify-center items-center h-100 bg-gray-300/30'>
-            <h1 className='text-4xl font-bold text-gray-800 mb-4'>Welcome to TechStore</h1>
+    <div className='w-full bg-white'>
+
+      <div className='flex flex-col justify-center items-center h-125 bg-[#F5F6FF] p-4'>
+        
+        <h1 className='text-[70px] font-bold text-gray-800 mb-4'>Welcome to TechStore</h1>
+
+        <p className='text-lg text-gray-600 max-w-md text-center'>
+          Discover the latest smartphones, tablets, audio devices, and accessories. Premium quality, unbeatable prices.
+        </p>
+
+        <div className='mt-4 flex  gap-4'>
+          <button className='flex gap-2 bg-black hover:bg-black/80 text-white py-2 px-4 rounded-lg transition-colors'>Shop Now <ArrowRight /> </button>
+          <button className='bg-gray-200 hover:bg-gray-300 text-gray-800 py-2 px-4 rounded-lg transition-colors'>View Phones</button>
+        </div>
+      </div>
+
+
+      <div className='flex justify-evenly h-70 gap-10 p-8 border-b border-white/20 shadow-lg'>
+
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <div className='bg-blue-100 p-4 rounded-full text-blue-600'><Zap /></div>
+          <h2 className='font-bold'>Fast Delivery</h2>
+          <p className='text-gray-600 text-center'>Get your products delivered in 2-3 business days</p>
         </div>
 
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <div className='bg-green-100 p-4 rounded-full text-green-600'><Shield /></div>
+          <h2 className='font-bold'>Secure Shopping</h2>
+          <p className='text-gray-600 text-center'>Your data is protected with industry-standard encryption</p>
+        </div>
 
-        <div></div>
-        <div></div>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <div className='bg-purple-100 p-4 rounded-full text-purple-600'><Truck /></div>
+          <h2 className='font-bold'>Free Returns</h2>
+          <p className='text-gray-600 text-center'>30-day return policy on all products</p>
+        </div>
+
+      </div>
+
+
+      <div className='bg-[#F7F7F9] h-auto flex flex-col justify-center items-center p-10'>
+
+        <h1 className='font-bold text-3xl'>Shop by Category</h1>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 justify-center mt-6 mb-4'>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+          <div className='box border hover:shadow-lg transition-shadow rounded-lg p-6 py-16'></div>
+        </div>
+
+      </div>
+
+      <div className='h-200 m-5'>
+          <div className='flex justify-between'>
+            <h1 className='font-bold text-3xl'>Featured Products</h1>
+            <p className='flex gap-2 cursor-pointer'>View All <ArrowRight /></p>
+          </div>
+      </div>
 
     </div>
   )
